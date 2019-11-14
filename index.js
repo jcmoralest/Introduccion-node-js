beba41b7031e7e8dce4be735ed2f2aa65b43bd78
OneBurritoAdmin <on3burr1to@gmail.com>
2019-11-14 00:20:19 -0500

    módulo sencillo con saludos

---
+++ index.js
@@ -0,0 +1,11 @@
+modules.exports = {
+    hola: function() {
+        console.log('Hola Putos!!');
+    },
+    holaPersonalizado: function(nombre){
+    console.log(`Hola ${ nombre}`);
+    },
+    holaAlCursoNPM: function(){
+        console.log('hola al curso NPM!!!');
+    }
+}
